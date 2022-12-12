9026ee3f3c8d758f1ebc7e937bccc172dacaa14d
JeiKeiLim <lim.jeikei@gmail.com>
2022-12-12 13:44:46 +0900

    Add custom configurations.

---
+++ lua/custom/init.lua
@@ -0,0 +1,18 @@
+-- local autocmd = vim.api.nvim_create_autocmd
+
+-- Auto resize panes when resizing nvim window
+-- autocmd("VimResized", {
+--   pattern = "*",
+--   command = "tabdo wincmd =",
+-- })
+
+-- custom/init.lua
+local enable_providers = {
+  "python3_provider",
+  "node_provider",
+}
+
+for _, plugin in pairs(enable_providers) do
+  vim.g["loaded_" .. plugin] = nil
+  vim.cmd("runtime " .. plugin)
+end
---
+++ lua/custom/plugins/null-ls.lua
@@ -0,0 +1,26 @@
+local present, null_ls = pcall(require, "null-ls")
+
+if not present then
+  return
+end
+
+local b = null_ls.builtins
+
+local sources = {
+  -- Shell
+  b.formatting.shfmt,
+  b.diagnostics.shellcheck.with { diagnostics_format = "#{m} [#{c}]" },
+
+  -- cpp
+  b.formatting.clang_format,
+  b.formatting.rustfmt,
+
+  -- python
+  b.diagnostics.flake8,
+  b.formatting.black,
+}
+
+null_ls.setup {
+  debug = true,
+  sources = sources,
+}
