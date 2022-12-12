28f0f7f8a640b07af89d178e4a595f0299cd26a4
JeiKeiLim <lim.jeikei@gmail.com>
2022-12-12 19:35:02 +0900

    Change default theme. Add custom key mapping.

--- lua/custom/mappings.lua
+++ lua/custom/mappings.lua
@@ -3,7 +3,8 @@ local M = {}
 M.general = {
   n = {
     [";"] = { ":", "command mode", opts = { nowait = true } },
-    ["@d"] = { "$a<CR>__import__('pdb').set_trace()<ESC>:w<CR>"}
+    ["@d"] = { "$a<CR>__import__('pdb').set_trace()<ESC>:w<CR>", "Insert python debugger pdb."},
+    [",w"] = { "<cmd> w <CR>", "Save file"}
   },
 }
 
