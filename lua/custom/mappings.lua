9cba072084c871e28df3378afd912cbd490e090e
JeiKeiLim <lim.jeikei@gmail.com>
2022-12-14 10:25:24 +0900

    Add switching tab shortcuts.

--- lua/custom/mappings.lua
+++ lua/custom/mappings.lua
@@ -3,7 +3,24 @@ local M = {}
 M.general = {
   n = {
     ["@d"] = { "$a<CR>__import__('pdb').set_trace()<ESC>:w<CR>", "Insert python debugger pdb."},
+    -- Because of my old habit.
     [",w"] = { "<cmd> w <CR>", "Save file"}
+  }
+}
+
+M.tabufline = {
+  n = {
+    -- Because of my old habit.
+    [",1"] = { "1gt", "Go to tab number 1" },
+    [",2"] = { "2gt", "Go to tab number 2" },
+    [",3"] = { "3gt", "Go to tab number 3" },
+    [",4"] = { "4gt", "Go to tab number 4" },
+    [",5"] = { "5gt", "Go to tab number 5" },
+    [",6"] = { "6gt", "Go to tab number 6" },
+    [",7"] = { "7gt", "Go to tab number 7" },
+    [",8"] = { "8gt", "Go to tab number 8" },
+    [",9"] = { "9gt", "Go to tab number 9" },
+    [",0"] = { "<cmd> tablast <CR>", "Go to the lat tab" },
   },
 }
 
