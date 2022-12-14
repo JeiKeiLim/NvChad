b0e6e669b1f7dd7fc0c3c47c2f0c69fc7c2e1b6c
JeiKeiLim <lim.jeikei@gmail.com>
2022-12-14 09:45:14 +0900

    Disable tab mapping in nvim-cmp for fixing conflict with Copilot. Remove ; mapping

--- lua/custom/mappings.lua
+++ lua/custom/mappings.lua
@@ -2,7 +2,6 @@ local M = {}
 
 M.general = {
   n = {
-    [";"] = { ":", "command mode", opts = { nowait = true } },
     ["@d"] = { "$a<CR>__import__('pdb').set_trace()<ESC>:w<CR>", "Insert python debugger pdb."},
     [",w"] = { "<cmd> w <CR>", "Save file"}
   },
