cf876ca50af7c6363fe55722fb069943d57a9c21
Jongkuk Lim <lim.jeikei@gmail.com>
2023-02-07 10:09:32 +0900

    Add golang support with gopls lsp server.

--- lua/custom/plugins/lspconfig.lua
+++ lua/custom/plugins/lspconfig.lua
@@ -3,7 +3,7 @@ local capabilities = require("plugins.configs.lspconfig").capabilities
 
 local lspconfig = require "lspconfig"
 
-local servers = { "pyright", "tsserver" }
+local servers = { "pyright", "tsserver", "gopls" }
 
 for _, lsp in ipairs(servers) do
   lspconfig[lsp].setup {
