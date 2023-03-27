047e8f07b23d5af90fd11ece3ea408158bf2dc90
JeiKeiLim <limjk@jmarple.ai>
2023-03-27 11:27:05 +0900

    Fix plugin load to v2.0

--- lua/custom/plugins.lua
+++ lua/custom/plugins.lua
@@ -1,55 +1,62 @@
 local overrides = require "custom.configs.overrides"
 
-return {
-
-  -- ["goolord/alpha-nvim"] = { disable = false } -- enables dashboard
+local plugins = {
 
   -- Override plugin definition options
-  ["neovim/nvim-lspconfig"] = {
+
+  {
+    "neovim/nvim-lspconfig",
+    dependencies = {
+      -- format & linting
+      {
+        "jose-elias-alvarez/null-ls.nvim",
+        config = function()
+          require "custom.configs.null-ls"
+        end,
+      },
+    },
     config = function()
       require "plugins.configs.lspconfig"
       require "custom.configs.lspconfig"
-    end,
+    end, -- Override to setup mason-lspconfig
   },
 
-  -- overrde plugin configs
-  ["nvim-treesitter/nvim-treesitter"] = {
-    override_options = overrides.treesitter,
+  -- override plugin configs
+  {
+    "williamboman/mason.nvim",
+    opts = overrides.mason
   },
 
-  ["williamboman/mason.nvim"] = {
-    override_options = overrides.mason,
+  {
+    "nvim-treesitter/nvim-treesitter",
+    opts = overrides.treesitter,
   },
 
-  ["kyazdani42/nvim-tree.lua"] = {
-    override_options = overrides.nvimtree,
+  {
+    "nvim-tree/nvim-tree.lua",
+    opts = overrides.nvimtree,
   },
 
   -- Install a plugin
-  ["max397574/better-escape.nvim"] = {
+  {
+    "max397574/better-escape.nvim",
     event = "InsertEnter",
     config = function()
       require("better_escape").setup()
     end,
   },
-
-  -- code formatting, linting etc
-  ["jose-elias-alvarez/null-ls.nvim"] = {
-    after = "nvim-lspconfig",
-    config = function()
-      require "custom.configs.null-ls"
-    end,
-  },
-  ["github/copilot.vim"] = {
+  {
+    "github/copilot.vim",
     branch = "release",
   },
-  ["folke/which-key.nvim"] = {
-    disable = false
+  {
+    "folke/which-key.nvim",
+    disable = false,
   },
-  ["NvChad/nvterm"] = {
-    override_options = overrides.nvterm,
+  {
+    "NvChad/nvterm",
+    opts = overrides.nvterm,
   }
-
-  -- remove plugin
-  -- ["hrsh7th/cmp-path"] = false,
 }
+
+return plugins
